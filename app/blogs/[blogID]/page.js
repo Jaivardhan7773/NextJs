1c0dfebda5dbced84f30070edc51467356da657e
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-06-22 23:44:22 +0530

    dynamic params intro nd basics

--- app/blogs/[blogID]/page.js
+++ app/blogs/[blogID]/page.js
@@ -1,3 +1,6 @@
+//these will only works in production mode, not in development mode
+
+
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) {
@@ -8,25 +11,53 @@ export async function generateMetadata({ params }) {
 }
 
 
+
+
+export const dynamicParams = false; //this disables dynamic params for this route, meaning it will not generate pages for every possible blogID;
+
+
+
+
+
+//this uses static site generation (SSG) to generate static pages for each blog post
 // This function generates static parameters for the dynamic route
 // It allows Next.js to pre-render pages for specific blog IDs
 // This is useful for SEO and performance, as it avoids server-side rendering for every request
 // Instead, it generates static pages for the specified blog IDs
 // The function returns an array of objects, each containing a blogID
 // This is useful for blogs that have a fixed number of posts or for testing purposes
-export function generateStaticParams(){
-  return [
-    { blogID: "1" },
-    { blogID: "2" },
-    { blogID: "3" },
-    { blogID: "4" },
-    { blogID: "5" },
-    { blogID: "6" },
-    { blogID: "7" },
-    { blogID: "8" },
-    { blogID: "9" },
-    { blogID: "10" }
-  ]
+// export function generateStaticParams(){
+//   return [
+//     { blogID: "1" },
+//     { blogID: "2" },
+//     { blogID: "3" },
+//     { blogID: "4" },
+//     { blogID: "5" },
+//     { blogID: "6" },
+//     { blogID: "7" },
+//     { blogID: "8" },
+//     { blogID: "9" },
+//     { blogID: "10" }
+//   ]
+// }
+
+
+
+//this uses incremental static regeneration (ISR) to generate static pages for each blog post
+// This function fetches blog data from an external API
+// It retrieves a list of blog posts and generates static parameters based on the post IDs
+// This is useful for blogs that have a dynamic number of posts or for fetching real-time data
+// The function returns an array of objects, each containing a blogID
+// This is useful for blogs that have a dynamic number of posts or for fetching real-time data
+// It allows Next.js to pre-render pages for specific blog IDs
+// This is useful for SEO and performance, as it avoids server-side rendering for every request 
+//this is uses 
+export async function generateStaticParams(){
+const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+const data = await response.json();
+
+  return data.map(({id}) => ({blogID: `${id}`}))
+
 }
 
 
