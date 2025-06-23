bdf2762632d684a450064684658c0ca0b1289e8a
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-06-23 22:55:01 +0530

    how to make a page dynamic

--- app/(marketing)/services/page.js
+++ app/(marketing)/services/page.js
@@ -1,11 +1,22 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 export const metadata = {
   title: "Services",
 };
 
+//how to make a page dynamic 
+// export const dynamic = "auto";
+// export const dynamic = "force-dynamic";
+// export const dynamic = "error";
+// export const dynamic = "force-static";
 
-export default function Services(){
+export default async function Services({  searchParams}){
+  //this is a example of how to make a static page dynamic with search params and cookies
+    // const search = await searchParams;
+  // console.log(search);
+  // const myCookies = await cookies();
+  // console.log(myCookies);
     return (
         <>
         <nav>
